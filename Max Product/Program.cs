82d5d072e91c71eda1ed21504c423ee5e9182d1e
Bhaskara-cheruku <bhaskara.cheruku@cognine.com>
2024-07-30 19:09:47 +0530

    Add files via upload

---
+++ Max Product/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Max_Product
+{
+    class Program{
+
+        /// <summary>
+        /// Entry Point for the project
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number to check : ");
+            string Value = Console.ReadLine();
+            Validations v = new Validations();
+            do
+            {
+                if (v.isValid(Value))
+                {
+                    int digit1, digit2, digit3, digit4;
+                    MaxProductFourDigits mpf = new MaxProductFourDigits();
+                    int Max_Product = mpf.maxProductFinder(Value, out digit1, out digit2, out digit3, out digit4);
+
+                    Console.WriteLine(digit1 + " * " + digit2 + " * " + digit3 + " * " + digit4 + " = " + Max_Product);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the correct number [0-9]");
+                    Console.WriteLine("Example : 2313573894");
+                    Console.WriteLine("Enter the number to check : ");
+                    string newValue = Console.ReadLine();
+                    Value = newValue;
+                    if (v.isValid(Value))
+                    {
+                        int digit1, digit2, digit3, digit4;
+                        MaxProductFourDigits mpf = new MaxProductFourDigits();
+                        int Max_Product = mpf.maxProductFinder(Value, out digit1, out digit2, out digit3, out digit4);
+                        Console.WriteLine(digit1 + " * " + digit2 + " * " + digit3 + " * " + digit4 + " = " + Max_Product);
+                    }
+                }
+            } while (!v.isValid(Value));
+
+        }
+    }
+}
---
+++ Max Product/Validations.cs
@@ -0,0 +1,40 @@
+﻿namespace Max_Product
+{
+    class Validations{
+
+        /// <summary>
+        /// Method to check the user input is valid or not.
+        /// </summary>
+        /// <param name="number">caontains the user input </param>
+        /// <returns></returns>
+        public bool isValid(string number)
+        {
+            int Count = 0;
+            //checking length of given input value
+            if (number.Length < 4 || number.Length==0)
+            {
+                return false;
+            }
+            //condition to check weather the given input is number is not!
+            for (int i = 0; i < number.Length; i++)
+            {
+                if ((int)number[i]<48 || (int)number[i]>57)
+                {
+                    Count++;
+                }
+
+                if ((int)number[i] == 45)
+                {
+                    Count--;
+                }
+            }
+            if (Count == 0) { return true; }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
